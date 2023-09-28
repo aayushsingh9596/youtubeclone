94592fa7d30c072baa23a113f406a47a6f7c1552
aayushsingh9596 <aayushsingh9596@gmail.com>
2023-09-28 10:36:19 +0530

    https issue

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./utils/store";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDS3XNB6TKp-Tk1MhYnZp2YynKrJLD0oK4",
+  authDomain: "clone-5a8a8.firebaseapp.com",
+  projectId: "clone-5a8a8",
+  storageBucket: "clone-5a8a8.appspot.com",
+  messagingSenderId: "898560456737",
+  appId: "1:898560456737:web:78805f941d955dbc098b60",
+  measurementId: "G-FM7K77BHT9"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,3 +23,6 @@ root.render(
      <Provider store={store}><App /></Provider>
   </React.StrictMode>
 );
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
